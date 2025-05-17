192c5ad3f63db08b16b3501fe090f382c619ed34
Nathan Huisman <huis.nathan@gmail.com>
2025-05-17 13:56:42 +0200

    Add editor command utils
    Make the following changes to make creating a menu for the editor
    easier:
    
    Editor now takes children, you'd use it as follows:
    ```tsx
    function App() {
        return (
          <Editor>
            <Menu />
          </Editor>
        );
    }
    ```
    
    Inside the Editor component, you can use the functions `useCommand`,
    which returns whether a command is available with the current editor
    state, or `dispatchCommand`, which will dispatch a command using the
    current editor state.
    
    You can also use `useEditorState` to get the current state of the
    editor.

--- app/src/components/Editor.tsx
+++ app/src/components/Editor.tsx
@@ -1,6 +1,6 @@
-import { Component, onCleanup, onMount } from "solid-js";
+import { Accessor, createContext, createMemo, createSignal, onCleanup, ParentComponent, untrack, useContext } from "solid-js";
 import type { Node, Schema } from "prosemirror-model";
-import { EditorState } from "prosemirror-state";
+import { Command, EditorState, Plugin } from "prosemirror-state";
 import { EditorView } from "prosemirror-view";
 import { keymap } from "prosemirror-keymap";
 import { history, redo, undo } from "prosemirror-history";
@@ -11,21 +11,66 @@ export interface EditorProps {
   initialDocument?: Node;
 }
 
-export const Editor: Component<EditorProps> = (props) => {
-  let ref: HTMLDivElement;
-  onMount(() => {
-    const state = EditorState.create({
-      schema: props.schema,
-      doc: props.initialDocument,
-      plugins: [
-        history(),
-        keymap({ "Mod-z": undo, "Mod-y": redo }),
-        keymap(baseKeymap),
-      ],
-    });
-    const view = new EditorView(ref!, { state });
-    onCleanup(() => view.destroy());
-  });
+const editorContext = createContext<{ state: Accessor<EditorState>, view: Accessor<EditorView> }>();
 
-  return <div ref={ref!} />;
+export function useEditorState() {
+  return useContext(editorContext)?.state;
+}
+
+export function useCommand(cmd: Command) {
+  const output = createMemo(() => {
+    const ctx = useContext(editorContext);
+    if (!ctx) throw new Error("Editor state used outside of editor context");
+
+    const res = cmd(ctx.state(), undefined, ctx.view());
+    console.log("a", res);
+    return res;
+  });
+  return output;
+}
+
+export function dispatchCommand(cmd: Command) {
+  const ctx = useContext(editorContext)
+  if (!ctx) throw new Error("Editor context used outside of editor");
+
+  const state = ctx.state();
+  const view = ctx.view();
+  return cmd(state, view.dispatch, view);
+}
+
+
+export const Editor: ParentComponent<EditorProps> = (props) => {
+  const [ref, setRef] = createSignal<HTMLDivElement>();
+  const editorState = createMemo(() => EditorState.create({
+    schema: props.schema,
+    doc: props.initialDocument,
+    plugins: [
+      history(),
+      keymap({ "Mod-z": undo, "Mod-y": redo }),
+      keymap(baseKeymap),
+      new Plugin({
+        view() {
+          return {
+            update(_view, prevState) {
+              setState(prevState);
+            },
+          }
+        },
+      })
+    ],
+  }));
+
+  // editorState is just used to initialize the value. Because this is not a reactive scope,
+  // this function never reruns. To make this explicit, solidjs recommends to use `untrack' here.
+  const [state, setState] = createSignal<EditorState>(untrack(editorState), { equals: false });
+
+  const view = createMemo(() => new EditorView(ref()!, { state: editorState() }));
+  onCleanup(() => view().destroy());
+
+  return <>
+    <editorContext.Provider value={{ state, view }}>
+      {props.children}
+    </editorContext.Provider>
+    <div ref={setRef} />
+  </>;
 };
