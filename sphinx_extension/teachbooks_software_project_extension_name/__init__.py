0edf4cb569d4c8d426bb79eacabba8a3580e9bc8
Leander Bindt <l.bindt@student.tudelft.nl>
2025-06-14 15:28:37 +0200

    Version update for proper release

--- sphinx_extension/teachbooks_software_project_extension_name/__init__.py
+++ sphinx_extension/teachbooks_software_project_extension_name/__init__.py
@@ -28,7 +28,7 @@ def setup(app: Sphinx):
     # Basic information of the sphinx extension
     # Match with myproject.toml
     return {
-        'version': '0.0.8',
+        'version': '0.1.0',
         'parallel_read_safe': True,
         'parallel_write_safe': True,
     }
