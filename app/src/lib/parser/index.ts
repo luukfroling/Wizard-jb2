d1637ff457ef47fe3b9efcf4337262cfb49f9683
luukfroling <L.Froling@student.tudelft.nl>
2025-12-20 13:44:22 +1100

    add console log - sanity check

--- app/src/lib/parser/index.ts
+++ app/src/lib/parser/index.ts
@@ -8,6 +8,7 @@ export {
 } from "./prosemirror_to_myst";
 
 export function parseMyst(source: string): Node {
+    console.log("PASSING MYST!!!!")
     const parsed = parseToMystAST(source);
     return mystToProseMirror(parsed);
 }
